962e772a2ab4951093f26f724b890b8ba270661b
zack junior <zacarias1992.12.19@gmail.com>
2023-10-02 13:23:30 -0300

    Product

--- src/main/java/com/zlab/course/config/TestConfig.java
+++ src/main/java/com/zlab/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.zlab.course.entities.Category;
 import com.zlab.course.entities.Order;
+import com.zlab.course.entities.Product;
 import com.zlab.course.entities.User;
 import com.zlab.course.entities.enums.OrderStatus;
 import com.zlab.course.repositories.CategoryRepository;
 import com.zlab.course.repositories.OrderRepository;
+import com.zlab.course.repositories.ProductRepository;
 import com.zlab.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,7 +41,14 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product (null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 98.5, "");
+		Product p2 = new Product (null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.8, ""); 
+		Product p3 = new Product (null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.8, "");
+		Product p4= new Product (null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1208.0, "");
+		Product p5= new Product (null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 108.99, "");
+		
 		categoryRepository.saveAll((Arrays.asList(cat1, cat2, cat3)));
+		productRepository.saveAll((Arrays.asList(p1, p2, p3, p4, p5)));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/zlab/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.zlab.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zlab.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
