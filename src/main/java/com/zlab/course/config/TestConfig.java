e82ef8ef2ac558867b3ef84106f800802df62d52
zack junior <zacarias1992.12.19@gmail.com>
2023-10-08 12:00:14 -0300

    Payment, One-to-One association

--- src/main/java/com/zlab/course/config/TestConfig.java
+++ src/main/java/com/zlab/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.zlab.course.entities.Category;
 import com.zlab.course.entities.Order;
 import com.zlab.course.entities.OrderItem;
+import com.zlab.course.entities.Payment;
 import com.zlab.course.entities.Product;
 import com.zlab.course.entities.User;
 import com.zlab.course.entities.enums.OrderStatus;
@@ -37,7 +38,7 @@ public class TestConfig implements CommandLineRunner {
 	private ProductRepository productRepository;
 
 	@Autowired
-	private OrderItemRepository orderItemtRepository;
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -79,7 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
-		orderItemtRepository.saveAll((Arrays.asList(oi1, oi2, oi3, oi4)));
+		orderItemRepository.saveAll((Arrays.asList(oi1, oi2, oi3, oi4)));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 }
